df10979bfacbc8c81f2f82c1efcf3bab5fcd1066
이성훈 <leeseonghoon@dondummy.local>
2025-07-18 11:39:56 +0900

    hoon

---
+++ lib/common/widgets/colors.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  // 연한 핑크색
+  static const Color lightPink = Color(0xFFFCC2D7);
+
+  // 핑크색
+  static const Color pink = Color(0xFFD6336C);
+
+  // 검정색
+  static const Color black = Color(0xFF212529);
+
+  // 회색
+  static const Color gray = Color(0xFF868E96);
+
+  // 연한 회색
+  static const Color lightGray = Color(0xFFCED4DA);
+}
