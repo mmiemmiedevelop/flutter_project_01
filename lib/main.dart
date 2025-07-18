1671a58d76d4fbbb48084ae19f974a253ee0bb27
wing72 <xowkddl@gmail.com>
2025-07-18 18:04:07 +0900

    fix 메인수정

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_project_01/screens/item_detail/item_detail_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -7,9 +6,24 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: const Item_Detil());
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Home Page')),
+      body: const Center(child: Text('Hello, Flutter!')),
+    );
   }
 }
