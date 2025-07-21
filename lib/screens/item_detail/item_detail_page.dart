2a476bd04cf3a8712d0129ab707fbb5298c02a4b
mmie <samlee.develop@gmail.com>
2025-07-21 14:38:00 +0900

    feat: 상품상세 페이지에서 장바구니 담기 기능 추가

--- lib/screens/item_detail/item_detail_page.dart
+++ lib/screens/item_detail/item_detail_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_project_01/screens/cart/cart_page.dart';
+import 'package:flutter_project_01/screens/cart/cart_utils/cart_singleton.dart';
 
 class Item_Detil extends StatefulWidget {
   const Item_Detil({super.key});
@@ -195,7 +197,12 @@ class _Item_DetilState extends State<Item_Detil> {
                     ),
                   ),
                   onPressed: () {
-                    Navigator.pop(context,);
+                    Cart().add(qty);//[Todo]싱글톤으로 수량만 해놓은 부분 수정필요
+                    Navigator.of(context).push(
+                      MaterialPageRoute(
+                        builder: (context) => const CartPage(),
+                      ),
+                    );
                   },
                 ),
               ],
